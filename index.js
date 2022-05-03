d91ad2343981a642fe32515d4910feae9a7f50dd
Gaurav Shinde <shindegauravs12@gmail.com>
2022-05-03 18:51:26 +0530

    changed https.get function

--- index.js
+++ index.js
@@ -16,33 +16,14 @@ app.post("/", function(req, res){
     const appid ="cda42f7250658a1b71b1be2a21958b39";
     const link = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid="+appid+"&units=metric";
     //using https to get / request data from external server
-    // https.get(link,function(response){
-    //     console.log(response.statusCode)
-    //     response.on("data",function(data){
-    //         let forcastinfo = JSON.parse(data)
+    https.get(link,function(response){
+        console.log(response.statusCode);
+        response.on("data",function(data){
+          const weatherdata = JSON.parse(data);
 
-    //     });
-    
-    // });
-    https.get(link , "JSON", function(response){
-        var data;
-        response.on("data", function(chunk) {
-          if (!data) {
-            data = chunk;
-          } else {
-            data += chunk;
-          }
         });
     
-        response.on("end", function() {
-            const forcastinfo=JSON.parse(data);
-            console.log(forcastinfo);
-
-            res.sendStatus(forcastinfo.hourly);
-        });
     });
-    
-
 });
 
 app.listen(process.env.PORT || 3000,function(){
