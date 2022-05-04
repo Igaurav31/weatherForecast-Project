94c1121e0b875069d3d29701099bd678440ca9d8
Gaurav Shinde <shindegauravs12@gmail.com>
2022-05-04 22:59:07 +0530

    made changes in js file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 const https = require("https");
 const { json } = require("express/lib/response");
+const fetch = require('node-fetch');
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/index.html");
@@ -16,14 +17,18 @@ app.post("/", function(req, res){
     const appid ="cda42f7250658a1b71b1be2a21958b39";
     const link = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&appid="+appid+"&units=metric";
     //using https to get / request data from external server
-    https.get(link,function(response){
-        console.log(response.statusCode);
-        response.on("data",function(data){
-          const weatherdata = JSON.parse(data);
+    // https.get(link,function(response){
+    //     console.log(response.statusCode);
+    //     response.on("data",function(data){
+    //       const weatherdata = JSON.parse(data);
 
-        });
+    //     });
     
-    });
+    // });
+    fetch(link)
+  .then(response => response.json());
+  .then(data => console.log(data));
+
 });
 
 app.listen(process.env.PORT || 3000,function(){
