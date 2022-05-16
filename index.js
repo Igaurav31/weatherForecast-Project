6e46fe6d5df78c378e52000471fc9a3018a4baa9
Gaurav Shinde <shindegauravs12@gmail.com>
2022-05-17 02:22:15 +0530

    -

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const https = require("https");
 const { json } = require("express/lib/response");
 const fetch = require('node-fetch');
 var _ = require('lodash');
-// const unixconvertor = require("./functions");
+const { time } = require("console");
 app.set('view engine', 'ejs');
 // const convertor = require(__dirname + "/functions.js");
 // const coordinates = [{lat:18.521428 , lon:73.8544541}]
 // var dataset = {"hello":"world"};
+
 
 app.get("/", function (req, res) {
   let pagename = "";
@@ -21,24 +22,20 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
   let cityname = req.body.city;        // get data  
   const appid = "cda42f7250658a1b71b1be2a21958b39";
+ 
   let coordinatelink = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityname + "&limit=1&appid=" + appid + "";
   fetch(coordinatelink)
     .then(response => response.json())
     .then(data => {
-      // const coords = [{lat : data[0].lat ,
-      // lon : data[0].lon
-      // }];
       let lat = data[0].lat;
       let lon = data[0].lon;
+     
+      
       const link = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + appid + "&units=metric";
       return fetch(link);
 
 
     })
-    // .catch(error=> {
-    //   document.getElementsByName('city').placeholder= "Please search for a valid city 😩"
-    //   console.error('There was an error!', error);
-    // })
     .then(responce => responce.json())
     .then(data => {
 
@@ -99,10 +96,18 @@ app.post("/", function (req, res) {
       let time16 = unixtotime(data.hourly[26].dt);
       let time17 = unixtotime(data.hourly[27].dt);
       let time18 = unixtotime(data.hourly[28].dt);
+      let time19 = unixtotime(data.hourly[29].dt);
+      let time20 = unixtotime(data.hourly[30].dt);
+      let time21 = unixtotime(data.hourly[31].dt);
+      let time22 = unixtotime(data.hourly[32].dt);
+      let time23 = unixtotime(data.hourly[33].dt);
+      let time24 = unixtotime(data.hourly[34].dt);
+      let humidity = data.current.humidity;
 
-      res.render("searchloc", { cityname: cityname, icon: iconurl,sunrise:sunrise,sunset:sunset,currenttemp: data.current.temp, description: data.current.weather[0].description, feels_like: data.current.feels_like, wind: data.current.wind_speed, visibility: data.current.visibility, pressure: data.current.pressure, dew_point: data.current.dew_point, uvi: data.current.uvi, day1: day1, day2: day2, day3: day3, day4: day4, day5: day5, day6: day6, day7: day7, day1icon: day1icon, day2icon: day2icon, day3icon: day3icon, day4icon: day4icon, day5icon: day5icon, day6icon: day6icon, day7icon: day7icon, day1maxtemp: data.daily[0].temp.max, day2maxtemp: data.daily[1].temp.max, day3maxtemp: data.daily[2].temp.max, day4maxtemp: data.daily[3].temp.max, day5maxtemp: data.daily[4].temp.max, day6maxtemp: data.daily[5].temp.max, day7maxtemp: data.daily[6].temp.max, day1mintemp: data.daily[0].temp.min, day2mintemp: data.daily[1].temp.min, day3mintemp: data.daily[2].temp.min, day4mintemp: data.daily[3].temp.min, day5mintemp: data.daily[4].temp.min, day6mintemp: data.daily[5].temp.min, day7mintemp: data.daily[6].temp.min, day1description: data.daily[0].weather[0].description, day2description: data.daily[1].weather[0].description, day3description: data.daily[2].weather[0].description, day4description: data.daily[3].weather[0].description, day5description: data.daily[4].weather[0].description, day6description: data.daily[5].weather[0].description, day7description: data.daily[6].weather[0].description, time1: time1, des1: data.hourly[11].weather[0].description, max1: data.hourly[11].temp, time2: time2, des2: data.hourly[12].weather[0].description, max2: data.hourly[12].temp, time3: time3, des3: data.hourly[13].weather[0].description, max3: data.hourly[13].temp, time4: time4, des4: data.hourly[14].weather[0].description, max4: data.hourly[14].temp, time5: time5, des5: data.hourly[12].weather[0].description, max5: data.hourly[15].temp, time6: time6, des6: data.hourly[16].weather[0].description, max6: data.hourly[16].temp, time7: time7, des7: data.hourly[17].weather[0].description, max7: data.hourly[17].temp, time8: time8, des8: data.hourly[18].weather[0].description, max8: data.hourly[18].temp, time9: time9, des9: data.hourly[19].weather[0].description, max9: data.hourly[19].temp, time10: time10, des10: data.hourly[20].weather[0].description, max10: data.hourly[20].temp, time11: time11, des11: data.hourly[21].weather[0].description, max11: data.hourly[21].temp, time12: time12, des12: data.hourly[22].weather[0].description, max12: data.hourly[22].temp, time13: time13, des13: data.hourly[23].weather[0].description, max13: data.hourly[23].temp, time14: time14, des14: data.hourly[24].weather[0].description, max14: data.hourly[12].temp, time15: time15, des15: data.hourly[25].weather[0].description, max15: data.hourly[25].temp, time16: time16, des16: data.hourly[26].weather[0].description, max16: data.hourly[26].temp, time17: time17, des17: data.hourly[27].weather[0].description, max17: data.hourly[27].temp, time18: time18, des18: data.hourly[28].weather[0].description, max18: data.hourly[28].temp });
+      res.render("searchloc", { cityname: cityname, icon: iconurl,sunrise:sunrise,sunset:sunset,currenttemp: data.current.temp, description: data.current.weather[0].description, feels_like: data.current.feels_like, wind: data.current.wind_speed, visibility: data.current.visibility, pressure: data.current.pressure, dew_point: data.current.dew_point, uvi: data.current.uvi, day1: day1, day2: day2, day3: day3, day4: day4, day5: day5, day6: day6, day7: day7, day1icon: day1icon, day2icon: day2icon, day3icon: day3icon, day4icon: day4icon, day5icon: day5icon, day6icon: day6icon, day7icon: day7icon, day1maxtemp: data.daily[0].temp.max, day2maxtemp: data.daily[1].temp.max, day3maxtemp: data.daily[2].temp.max, day4maxtemp: data.daily[3].temp.max, day5maxtemp: data.daily[4].temp.max, day6maxtemp: data.daily[5].temp.max, day7maxtemp: data.daily[6].temp.max, day1mintemp: data.daily[0].temp.min, day2mintemp: data.daily[1].temp.min, day3mintemp: data.daily[2].temp.min, day4mintemp: data.daily[3].temp.min, day5mintemp: data.daily[4].temp.min, day6mintemp: data.daily[5].temp.min, day7mintemp: data.daily[6].temp.min, day1description: data.daily[0].weather[0].description, day2description: data.daily[1].weather[0].description, day3description: data.daily[2].weather[0].description, day4description: data.daily[3].weather[0].description, day5description: data.daily[4].weather[0].description, day6description: data.daily[5].weather[0].description, day7description: data.daily[6].weather[0].description, time1: time1, des1: data.hourly[11].weather[0].description, max1: data.hourly[11].temp, time2: time2, des2: data.hourly[12].weather[0].description, max2: data.hourly[12].temp, time3: time3, des3: data.hourly[13].weather[0].description, max3: data.hourly[13].temp, time4: time4, des4: data.hourly[14].weather[0].description, max4: data.hourly[14].temp, time5: time5, des5: data.hourly[12].weather[0].description, max5: data.hourly[15].temp, time6: time6, des6: data.hourly[16].weather[0].description, max6: data.hourly[16].temp, time7: time7, des7: data.hourly[17].weather[0].description, max7: data.hourly[17].temp, time8: time8, des8: data.hourly[18].weather[0].description, max8: data.hourly[18].temp, time9: time9, des9: data.hourly[19].weather[0].description, max9: data.hourly[19].temp, time10: time10, des10: data.hourly[20].weather[0].description, max10: data.hourly[20].temp, time11: time11, des11: data.hourly[21].weather[0].description, max11: data.hourly[21].temp, time12: time12, des12: data.hourly[22].weather[0].description, max12: data.hourly[22].temp, time13: time13, des13: data.hourly[23].weather[0].description, max13: data.hourly[23].temp, time14: time14, des14: data.hourly[24].weather[0].description, max14: data.hourly[12].temp, time15: time15, des15: data.hourly[25].weather[0].description, max15: data.hourly[25].temp, time16: time16, des16: data.hourly[26].weather[0].description, max16: data.hourly[26].temp, time17: time17, des17: data.hourly[27].weather[0].description, max17: data.hourly[27].temp, time18: time18, des18: data.hourly[28].weather[0].description, max18: data.hourly[28].temp, time19: time19, des19: data.hourly[29].weather[0].description, max19: data.hourly[29].temp, time20: time20, des20: data.hourly[30].weather[0].description, max20: data.hourly[30].temp, time21: time21, des21: data.hourly[31].weather[0].description, max21: data.hourly[31].temp, time22: time22, des22: data.hourly[32].weather[0].description, max22: data.hourly[32].temp, time23: time23, des23: data.hourly[33].weather[0].description, max23: data.hourly[32].temp, time24: time24, des24: data.hourly[33].weather[0].description, max24: data.hourly[33].temp,humidity:humidity});
 
     })
+    
    
 
 });
@@ -110,7 +115,10 @@ app.post("/", function (req, res) {
 
 // app.get("/searchloc",function(req,res){
 
-
+ // .catch(error=> {
+    //   document.getElementsByName('city').placeholder= "Please search for a valid city 😩"
+    //   console.error('There was an error!', error);
+    // })
 
 // })
 
